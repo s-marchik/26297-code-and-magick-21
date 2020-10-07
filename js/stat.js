4d308af3f1ca12539802b451d8d9632215e4d84e
s-marchik <65153265+s-marchik@users.noreply.github.com>
2020-10-07 22:28:02 +0300

    Учебный проект: холст

--- js/stat.js
+++ js/stat.js
@@ -10,94 +10,80 @@ const BAR_GAP = 50;
 const TEXT_HEIGHT = 20;
 const BAR_WIDTH = 40;
 const BAR_HEIGHT_MAX = 150;
-const COLOR_SHADOW = 'rgba(0, 0, 0, 0.7)';
-const COLOR_BASIС = '#fff';
-const COLOR_BASIС_BLACK = '#000';
-const COLOR_MAIN_PLAYER = 'rgba(255, 0, 0, 1)';
-const COLOR_OTHERS_PLAYERS = '#4169e1';
+const COLOR_SHADOW = `rgba(0, 0, 0, 0.7)`;
+const COLOR_BASIС = `#fff`;
+const COLOR_BASIС_BLACK = `#000`;
+const COLOR_MAIN_PLAYER = `rgba(255, 0, 0, 1)`;
 
 const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx) {
+let getMaxElement = function (arr) {
+  let maxElement = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(
-    ctx,
-    CLOUD_X + GAP_SHADOW,
-    CLOUD_Y + GAP_SHADOW,
-    COLOR_SHADOW
+      ctx,
+      CLOUD_X + GAP_SHADOW,
+      CLOUD_Y + GAP_SHADOW,
+      COLOR_SHADOW
   );
   renderCloud(
-    ctx,
-    CLOUD_X,
-    CLOUD_Y,
-    COLOR_BASIС
+      ctx,
+      CLOUD_X,
+      CLOUD_Y,
+      COLOR_BASIС
   );
 
-ctx.font = '16px PT Mono';
-ctx.textBaseline = 'hanging'
-ctx.fillStyle = COLOR_BASIС_BLACK;
-ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP);
-ctx.fillText('Список результатов:',CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT);
+  ctx.font = `16px PT Mono`;
+  ctx.textBaseline = `hanging`;
+  ctx.fillStyle = COLOR_BASIС_BLACK;
+  ctx.fillText(`Ура вы победили!`, CLOUD_X + GAP, CLOUD_Y + GAP);
+  ctx.fillText(`Список результатов:`, CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT);
 
-ctx.fillStyle = COLOR_BASIС_BLACK;
-ctx.fillText(
-  'Вы',
-  CLOUD_X + 2*GAP,
-  CLOUD_HEIGHT - GAP
-);
+  let maxTime = getMaxElement(times);
 
-ctx.fillStyle = COLOR_MAIN_PLAYER;
-ctx.fillRect(
-  CLOUD_X + 2*GAP,
-  CLOUD_Y + (3 * GAP) + TEXT_HEIGHT,
-  BAR_WIDTH,
-  BAR_HEIGHT_MAX
-);
+  for (let i = 0; i < names.length; i++) {
+    let time = Number.parseInt(times[i], 10);
 
-ctx.fillStyle = COLOR_BASIС_BLACK;
-ctx.fillText(
-  'Кекс',
-  CLOUD_X + 2*GAP + BAR_GAP + BAR_WIDTH,
-  CLOUD_HEIGHT - GAP
-);
+    if (names[i] === `Вы`) {
+      ctx.fillStyle = COLOR_MAIN_PLAYER;
+    } else {
+      let saturation = Math.floor(Math.random() * 101);
+      ctx.fillStyle = `hsl(244, ` + saturation + `%, 46%)`;
+    }
 
-ctx.fillStyle = COLOR_OTHERS_PLAYERS;
-ctx.fillRect(
-  CLOUD_X + 2*GAP + BAR_GAP  + BAR_WIDTH,
-  CLOUD_Y + (3 * GAP) + TEXT_HEIGHT,
-  BAR_WIDTH,
-  BAR_HEIGHT_MAX
-);
+    let heightBar = (BAR_HEIGHT_MAX * time) / maxTime;
 
-ctx.fillStyle = COLOR_BASIС_BLACK;
-ctx.fillText(
-  'Катя',
-  CLOUD_X + 2*GAP + (2*(BAR_GAP + BAR_WIDTH)),
-  CLOUD_HEIGHT - GAP
-);
+    ctx.fillRect(
+        CLOUD_X + 2 * GAP + (BAR_GAP + BAR_WIDTH) * i,
+        CLOUD_HEIGHT - TEXT_HEIGHT - heightBar - GAP_SHADOW,
+        BAR_WIDTH,
+        heightBar
+    );
 
-ctx.fillStyle = COLOR_OTHERS_PLAYERS;
-ctx.fillRect(
-  CLOUD_X + 2*GAP + (2*(BAR_GAP + BAR_WIDTH)),
-  CLOUD_Y + (3 * GAP) + TEXT_HEIGHT,
-  BAR_WIDTH,
-  BAR_HEIGHT_MAX
-);
+    ctx.fillStyle = COLOR_BASIС_BLACK;
+    ctx.textBaseline = `hanging`;
+    ctx.fillText(
+        names[i],
+        CLOUD_X + 2 * GAP + (BAR_GAP + BAR_WIDTH) * i,
+        CLOUD_HEIGHT - GAP
+    );
 
-ctx.fillStyle = COLOR_BASIС_BLACK;
-ctx.fillText(
-  'Игорь',
-  CLOUD_X + 2*GAP + (3*(BAR_GAP + BAR_WIDTH)),
-  CLOUD_HEIGHT - GAP
-);
-
-ctx.fillStyle = COLOR_OTHERS_PLAYERS;
-ctx.fillRect(
-  CLOUD_X + 2*GAP + (3*(BAR_GAP + BAR_WIDTH)),
-  CLOUD_Y + (3 * GAP) + TEXT_HEIGHT,
-  BAR_WIDTH,
-  BAR_HEIGHT_MAX
-);
-}
+    ctx.fillText(
+        time,
+        CLOUD_X + 2 * GAP + (BAR_WIDTH + BAR_GAP) * i,
+        CLOUD_HEIGHT - GAP_SHADOW - TEXT_HEIGHT - heightBar - TEXT_HEIGHT
+    );
+  }
+};
